f2baf3d2cdbcc14b6b1b62e5c4b3b71cda962d5d
Ernesto <ernestomiquel@gmail.com>
2020-06-08 20:32:09 -0300

    metiendo un panorama

---
+++ site/static/prueba_panorama/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main-qimg-b3bfc7b23beb53e2c81195fe64b22add",
+      "name": "main-qimg-b3bfc7b23beb53e2c81195fe64b22add",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 150.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
