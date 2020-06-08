611d2adb62076ca94f2c1b48a3781d2657f7a45c
Ernesto <ernestomiquel@gmail.com>
2020-06-08 20:41:28 -0300

    agrego botones

--- site/static/prueba_panorama/data.js
+++ site/static/prueba_panorama/data.js
@@ -20,8 +20,34 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": 0.006313377401969689,
+          "pitch": 0.16779744445372557,
+          "rotation": 0,
+          "target": "0-main-qimg-b3bfc7b23beb53e2c81195fe64b22add"
+        },
+        {
+          "yaw": 1.4795571878806504,
+          "pitch": 0.11198041113350499,
+          "rotation": 0,
+          "target": "0-main-qimg-b3bfc7b23beb53e2c81195fe64b22add"
+        },
+        {
+          "yaw": -2.813462287084093,
+          "pitch": -0.10534432312933717,
+          "rotation": 0,
+          "target": "0-main-qimg-b3bfc7b23beb53e2c81195fe64b22add"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6653906751537058,
+          "pitch": 0.11470729772971033,
+          "title": "aca iria informacion",
+          "text": "bla bla"
+        }
+      ]
     }
   ],
   "name": "Project Title",
@@ -29,6 +55,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": false,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
